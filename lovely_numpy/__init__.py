d7fb85f2f10d945d674cfc5c7739dfa6c740ae52
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2022-11-17 21:14:44 +0700

    Implement summary

--- lovely_numpy/__init__.py
+++ lovely_numpy/__init__.py
@@ -1,1 +1,7 @@
 __version__ = "0.0.1"
+
+from .repr_str import *
+# from lovely_tensors.repr_rgb import *
+# from lovely_tensors.repr_plt import *
+# from lovely_tensors.repr_chans import *
+# from lovely_tensors.patch import *
