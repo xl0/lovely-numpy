e16b6ae25f441b310578cd3ca5bdbc994d4fdaf2
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2022-11-18 20:49:34 +0700

    Pull monkey_patch into modules top-level

--- lovely_numpy/__init__.py
+++ lovely_numpy/__init__.py
@@ -4,4 +4,4 @@ from .repr_str import *
 # from lovely_tensors.repr_rgb import *
 # from lovely_tensors.repr_plt import *
 # from lovely_tensors.repr_chans import *
-# from lovely_tensors.patch import *
+from lovely_numpy.patch import *
