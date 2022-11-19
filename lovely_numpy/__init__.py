23bfafdc62931ac9d0b204749549af4f9be5e938
Alexey Zaytsev <alexey.zaytsev@gmail.com>
2022-11-19 16:56:00 +0700

    Clean up, expand README

--- lovely_numpy/__init__.py
+++ lovely_numpy/__init__.py
@@ -3,5 +3,5 @@ __version__ = "0.0.1"
 from .repr_str import *
 from .repr_rgb import *
 # from lovely_tensors.repr_plt import *
-# from lovely_tensors.repr_chans import *
+from .repr_chans import *
 from .patch import *
